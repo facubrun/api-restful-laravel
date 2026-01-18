9943c98da1550f1c7aacaf684830b478ae7feb10
Facundo Brun <facundobrun0303@gmail.com>
2026-01-18 16:26:44 -0300

    middleware for handle errors

--- basketclub/routes/api.php
+++ basketclub/routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\PlayerController;
+use App\Http\Middleware\ApiForceAcceptHeader;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/players', [PlayerController::class, 'index']);
+// listado de players
+Route::get('/players', [PlayerController::class, 'index'])
+    ->middleware([ApiForceAcceptHeader::class]);
