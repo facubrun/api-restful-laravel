59d491dc5fb241622b8cf9228f83b02438b562cb
Facundo Brun <facundobrun0303@gmail.com>
2026-01-18 17:23:26 -0300

    get & post

--- basketclub/app/Http/Controllers/PlayerController.php
+++ basketclub/app/Http/Controllers/PlayerController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\Player;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class PlayerController extends Controller
@@ -15,5 +16,47 @@ class PlayerController extends Controller
     {
         $players = Player::all();
         return response()->json($players, 200);
+    }
+
+    /**
+     * Show a player by id.
+     */
+    public function show(int $id) 
+    {
+        $player = Player::find($id);
+        if($player) {
+            return response()->json($player, 200);
+        } else {
+            return response()->json(['message' => 'Player not found'], 404);
+        }
+    }
+
+    /**
+     * Store a new player in the database.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'first_name' => 'required|max:128',
+            'last_name' => 'required|max:128',
+            'gender' => 'required|in:Male,Female,Other',
+            'date_birth' => 'required|date|before:'. Carbon::now()->subYears(6),
+        ]);
+
+        $player = new Player();
+
+        $player->first_name = $validated['first_name'];
+        $player->last_name = $validated['last_name'];
+        $player->gender = $validated['gender'];
+        $player->date_birth = $validated['date_birth'];
+
+        $player->save();
+
+        $data = [
+            'message' => 'Player created successfully',
+            'player' => $player
+        ];
+
+        return response()->json($data, 201);
     }
 }
--- basketclub/routes/api.php
+++ basketclub/routes/api.php
@@ -3,12 +3,19 @@
 use App\Http\Controllers\PlayerController;
 use App\Http\Controllers\TeamController;
 use App\Http\Middleware\ApiForceAcceptHeader;
+use App\Models\Player;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// listado de players
-Route::get('/players', [PlayerController::class, 'index'])
-    ->middleware([ApiForceAcceptHeader::class]);
+Route::middleware([ApiForceAcceptHeader::class])->group(function () {
+    // listado de players
+Route::get('/players', [PlayerController::class, 'index']);
+// busqueda de player por id
+Route::get('/players/{id}', [PlayerController::class, 'show']);
+// alta de nuevo player
+Route::post('/players', [PlayerController::class, 'store']);
+});
 
+// listado de teams    
 Route::get('/teams', [TeamController::class, 'index'])
     ->middleware([ApiForceAcceptHeader::class]);
